7ac33c2974e537578130cd8db997834799b965f2
Adila Safitri <jiads1207@gmail.com>
2025-09-14 17:27:56 +0700

    Add files via upload

---
+++ Tungtung.js
@@ -0,0 +1,6 @@
+var yakin = confirm("Mau dilanjuutt???");
+if (yakin) {
+  alert('Silahkan di isi untuk melanjutkan ceritanya yaa..');
+} else {
+  window.location = "https://www.instagram.com/yg_treasure_official?igsh=NWI0MG03c21zN3pn";
+}
